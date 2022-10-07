bc100d30237c497aca097421a6a70f48c77a934e
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-10-07 17:38:35 -0600

    match card working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const cards = document.querySelectorAll('.card')
-
+let isFirstChoiceClicked = false
+let firstCard
 const flipCard = (target) => {
     target.classList.remove('card-back')
 }
@@ -10,10 +11,29 @@ const flipCardBack = (target) => {
 const onCardClicked = (e) => {
     const target = e.target
     flipCard(target)
-    setTimeout(() => {
-        flipCardBack(target)
-    }, 2000)
+    if (isFirstChoiceClicked) {
+        if (target.innerText.trim() === firstCard.innerText.trim()) {
+            console.log('1')
+            target.classList.add('match-card')
+            firstCard.classList.add('match-card')
+        } else {
+            console.log('not 1')
+            setTimeout(() => {
+                flipCardBack(target)
+            }, 2000)
+        }
+        isFirstChoiceClicked = false
+        firstCard = undefined
+    } else {
+        isFirstChoiceClicked = true
+        firstCard = target
+        
+    }
 }
+
+
+
+
 
 cards.forEach((card) => {
     card.addEventListener('click', onCardClicked)
